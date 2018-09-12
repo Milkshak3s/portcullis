74d5e0e14a3a21f27b5ce654f66f132a4385d416
Chris <justchris.vantine@gmail.com>
2018-09-12 00:18:02 -0400

    Set some kube settings

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,17 @@
 # Exit on any error
 set -e
 
+# Setup basic gcloud config
+gcloud --quiet config set container/cluster $KUBE_CLUSTER
+gcloud --quiet container clusters get-credentials $KUBE_CLUSTER
+
+# Do the docker
 docker build -t "portcullis:$CIRCLE_SHA1" .
 docker tag "portcullis:$CIRCLE_SHA1" "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
 docker push "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
 
+# kube time
+kubectl config set-context $(kubectl config current-context)
+kubectl config view
+kubectl config current-context
 kubectl patch deployment portcullis -p '{"spec":{"template":{"spec":{"containers":[{"name":"portcullis","image":"us.gcr.io/'"${GOOGLE_PROJECT_ID}"'/portcullis:'"$CIRCLE_SHA1"'"}]}}}}'
