cf9b08eb2a94b917075af2a04ba7c53b6642d373
Chris <justchris.vantine@gmail.com>
2018-09-12 00:06:54 -0400

    Removed useless chown

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ set -e
 docker build -t "portcullis:$CIRCLE_SHA1" .
 docker tag "portcullis:$CIRCLE_SHA1" "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
 docker push "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
-chown -R ubuntu:ubuntu /home/ubuntu/.kube
+
 kubectl patch deployment portcullis -p '{"spec":{"template":{"spec":{"containers":[{"name":"portcullis","image":"us.gcr.io/'"${GOOGLE_PROJECT_ID}"'/portcullis:'"$CIRCLE_SHA1"'"}]}}}}'
