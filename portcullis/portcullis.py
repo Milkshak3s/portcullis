e4124f6f54b18dd29a9ccfd7551ce13451b03d22
Chris Vantine <justchris.vantine@gmail.com>
2018-09-12 01:06:01 -0400

    Fixed bad query reference

--- portcullis/portcullis.py
+++ portcullis/portcullis.py
@@ -64,7 +64,7 @@ def verify_password(username_or_token, password):
     user = User.verify_auth_token(username_or_token)
     if not user:
         # try user/pass auth
-        user = User.query.filter_by(username=username_or_token)
+        user = User.query.filter_by(username=username_or_token).first()
         if not user or not user.verify_password(password):
             return False
 
