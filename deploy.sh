37684f8f3dcd6a3fe4a436ec22d105a7f1aabfbe
Chris <justchris.vantine@gmail.com>
2018-09-12 00:02:42 -0400

    Try setup_remote_docker

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 # Exit on any error
 set -e
 
-# docker build -t "portcullis:$CIRCLE_SHA1" .
+docker build -t "portcullis:$CIRCLE_SHA1" .
 docker tag "portcullis:$CIRCLE_SHA1" "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
 docker push "us.gcr.io/${GOOGLE_PROJECT_ID}/portcullis:$CIRCLE_SHA1"
 chown -R ubuntu:ubuntu /home/ubuntu/.kube
