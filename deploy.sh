bc8f5f746fe45fe7fa2e3a5b8b7d4c20a9d42697
Chris <justchris.vantine@gmail.com>
2018-09-11 23:31:50 -0400

    Removed sudo. Again.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/portcullis
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+/opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/portcullis
+chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment portcullis -p '{"spec":{"template":{"spec":{"containers":[{"name":"portcullis","image":"us.gcr.io/${PROJECT_NAME}/portcullis:'"$CIRCLE_SHA1"'"}]}}}}'
