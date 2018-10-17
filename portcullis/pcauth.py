598c7264411f75c5ad8013509ee5ae822467af0b
Chris Vantine <justchris.vantine@gmail.com>
2018-10-17 10:24:14 -0400

    Added decorator for permission-based auth

--- portcullis/pcauth.py
+++ portcullis/pcauth.py
@@ -24,37 +24,86 @@ class PortcullisAuth():
         self._auth_path_rec = "/port/auth/resource"
         self._auth_path_perm = "/port/auth/permission"
 
-    def recauth(self, f):
+    def recauth(self, recpath=None):
         """
         Check if the auth'd user has access to the resource path requested
         """
-        @wraps(f)
-        def decorated(*args, **kwargs):
-            auth_header = request.headers.get('Authorization')
-            if auth_header is None:
-                # if there's no Auth header, they're not using a token
-                return {'error': 'missing auth token'}
+        def decorator(f):
+            @wraps(f)
+            def decorated_function(*args, **kwargs):
+                recpath_inner = recpath
+                if recpath_inner is None:
+                    # if a resource path isn't specified, use path in request
+                    recpath_inner = request.path
 
-            url = self._request_server + self._auth_path_rec
-            headers = {'Authorization': auth_header}
-            payload = {'resource_path': request.path}
-            response = requests.request("POST", url, json=payload, headers=headers)
+                auth_header = request.headers.get('Authorization')
+                if auth_header is None:
+                    # if there's no Auth header, they're not using a token
+                    return {'error': 'missing auth token'}
 
-            try:
-                r_data = response.json()
-            except:
-                # probably unauthed access or internal server error
-                return {'error': 'unexpected response from auth server'}
+                url = self._request_server + self._auth_path_rec
+                headers = {'Authorization': auth_header}
+                payload = {'resource_path': recpath_inner}
+                response = requests.request("POST", url, json=payload, headers=headers)
 
-            has_access = r_data.get('has_access')
-            if has_access is False:
-                # auth server returned False
-                return {'error': 'bad permissions'}
-            elif has_access is None:
-                # auth server returned an error probably
-                return {'error': 'unexpected response from auth server'}
+                try:
+                    r_data = response.json()
+                except:
+                    # probably unauthed access or internal server error
+                    return {'error': 'unexpected response from auth server'}
 
-            # at this point, success!
-            return f(*args, **kwargs)
+                has_access = r_data.get('has_access')
+                if has_access is False:
+                    # auth server returned False
+                    return {'error': 'bad permissions'}
+                elif has_access is None:
+                    # auth server returned an error probably
+                    return {'error': 'unexpected response from auth server'}
 
-        return decorated
+                # at this point, success!
+                return f(*args, **kwargs)
+
+            return decorated_function
+
+        return decorator
+
+    def permauth(self, perm=None):
+        """
+        Check if the auth'd user has access to the permission specified
+        """
+        def decorator(f):
+            @wraps(f)
+            def decorated_function(*args, **kwargs):
+                if perm is None:
+                    return {'error': 'no permission specified'}
+
+                auth_header = request.headers.get('Authorization')
+                if auth_header is None:
+                    # if there's no Auth header, they're not using a token
+                    return {'error': 'missing auth token'}
+
+                url = self._request_server + self._auth_path_perm
+                headers = {'Authorization': auth_header}
+                payload = {'perm_name': perm}
+                response = requests.request("POST", url, json=payload, headers=headers)
+
+                try:
+                    r_data = response.json()
+                except:
+                    # probably unauthed access or internal server error
+                    return {'error': 'unexpected response from auth server'}
+
+                has_access = r_data.get('has_access')
+                if has_access is False:
+                    # auth server returned False
+                    return {'error': 'bad permissions'}
+                elif has_access is None:
+                    # auth server returned an error probably
+                    return {'error': 'unexpected response from auth server'}
+
+                # at this point, success!
+                return f(*args, **kwargs)
+
+            return decorated_function
+
+        return decorator
